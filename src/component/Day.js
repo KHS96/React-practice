5433ceb19bf036f3c2a95e94112d8d80e5fcce26
KHS96 <7heesu7@hanmail.net>
2023-03-04 20:54:17 +0900

    next day, prev day 구현

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 export default function Day(){
     const { day } = useParams();    
     // const [words, setWords] = useState([]);
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
+    const days = useFetch("http://localhost:3001/days");
+    const prev_day = Number({day}.day)-1;
+    const next_day = Number({day}.day)+1;
 
     // useEffect(()=>{
     //     fetch(`http://localhost:3001/words?day=${day}`)
@@ -22,6 +25,12 @@ export default function Day(){
 
     return <>
     <h2>Day {day}</h2>
+    <div>
+        { prev_day >= 1 &&  <Link to={`/day/${prev_day}`} className="link">이전 </Link>}
+        { next_day <= days.length  && <Link to={`/day/${next_day}`} className="link">다음 </Link>}
+        {/* <Link to={`/day/${prev_day}`} className="link">이전 </Link> */}
+        {/* <Link to={`/day/${next_day}`} className="link">다음 </Link> */}
+    </div>
     {words.length === 0 && (<span>Loading...</span>)}
     <table>
         <tbody>
