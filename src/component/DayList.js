0f58fb024902d3c296c09ba3889af0fe1f79aa4e
KHS96 <7heesu7@hanmail.net>
2023-03-04 20:08:44 +0900

    느린 환경 다중요청 처리

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -5,6 +5,10 @@ export default function DayList(){
 
     const days = useFetch("http://localhost:3001/days");
 
+    if(days.length === 0){
+        return <span>Loading...</span>
+    }
+
     return (
         <>
         <ul className="list_day">
