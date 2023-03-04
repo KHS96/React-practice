d4435d8e00b76e5ffbaffe75e88c35e28afecff5
KHS96 <7heesu7@hanmail.net>
2023-03-04 23:23:58 +0900

    day delete 구현

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 export default function Day(){
@@ -7,29 +7,32 @@ export default function Day(){
     // const [words, setWords] = useState([]);
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
     const days = useFetch("http://localhost:3001/days");
+    const history = useHistory();
     const prev_day = Number({day}.day)-1;
     const next_day = Number({day}.day)+1;
-
-    // useEffect(()=>{
-    //     fetch(`http://localhost:3001/words?day=${day}`)
-    //     .then(res=>{
-    //         return res.json();
-    //     })
-    //     .then(data=>{
-    //         setWords(data);
-    //     })
-    // },[day])
-
-
-    // console.log(wordList);
+    // console.log(words.length);
+    function del(){
+        if(window.confirm("삭제 하시겠습니까?")){
+        if(words.length!=0){
+            console.log({words});
+            {words.map(word =>(
+                fetch(`http://localhost:3001/words/${word.id}` , {
+                    method: "DELETE"
+                })
+                .then(history.push("/"))
+                )
+            )}
+            }
+        }
+    }
+     
 
     return <>
     <h2>Day {day}</h2>
+    <button onClick={del}>Day 삭제</button>
     <div>
         { prev_day >= 1 &&  <Link to={`/day/${prev_day}`} className="link">이전 </Link>}
         { next_day <= days.length  && <Link to={`/day/${next_day}`} className="link">다음 </Link>}
-        {/* <Link to={`/day/${prev_day}`} className="link">이전 </Link> */}
-        {/* <Link to={`/day/${next_day}`} className="link">다음 </Link> */}
     </div>
     {words.length === 0 && (<span>Loading...</span>)}
     <table>
