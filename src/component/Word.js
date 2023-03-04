aa5dfbfab1b7b4beca9b51a3bcdadba962831d34
KHS96 <7heesu7@hanmail.net>
2023-03-04 19:33:44 +0900

    delete , update(isDone) 구현

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,6 +1,7 @@
 import {useState} from "react"
 
-export default function Word({word}) {
+export default function Word({word: w}) {
+    const [word, setWord] = useState(w);
     const [isShow, setIsShow] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone);
 
@@ -9,9 +10,40 @@ export default function Word({word}) {
     }
 
     function toggleDone() {
-        setIsDone(!isDone);
+        // setIsDone(!isDone);
+        fetch(`http://localhost:3001/words/${word.id}` , {
+            method: 'PUT',
+            headers : {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify({
+                ...word,
+                isDone : !isDone
+            }),
+        })
+        .then(res =>{
+            if(res.ok){
+                setIsDone(!isDone);
+            }
+        });
     }
 
+    function del(){
+        if(window.confirm("삭제 하시겠습니까?")){
+            fetch(`http://localhost:3001/words/${word.id}` , {
+                method: "DELETE"
+            }).then(res => {
+                if(res.ok){
+                    setWord({id:0});
+                }
+            })
+        }
+
+    }
+
+    if(word.id === 0){
+        return null;
+    }
 
     return (
         <tr className={isDone ? "off" : ""}>
@@ -26,7 +58,7 @@ export default function Word({word}) {
                             : "보기"
                     }
                 </button>
-                <button className="btn_del">삭제</button>
+                <button className="btn_del" onClick={del}>삭제</button>
             </td>
         </tr>
     )
